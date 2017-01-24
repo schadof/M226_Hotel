1d3bbbeddc75d4940725c7329258853e447d4c5c
schadof <lars.ragutt@gmx.ch>
2017-01-24 16:45:05 +0100

    Just some classes

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Player player = new Player();
+        Menu menu = new Menu();
         System.out.println("Welcome to Trails of Agony");
         Scanner in = new Scanner(System.in);
         System.out.println("Name of your adventurer");
-        Menu menu = new Menu();
-        menu.menu(in.nextLine());
+        player.name = in.nextLine();
+        System.out.println("Hello brave " + player.name);
+        menu.menu(player);
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,10 +1,6 @@
 public class Menu {
-    String name;
 
-    public void menu(String name) {
-        Player player = new Player();
-        player.name = name;
-        this.name = name;
-        System.out.println("Hello brave " + name);
+    public void menu(Object player) {
+
     }
 }
