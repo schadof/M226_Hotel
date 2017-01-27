7adac7bb85fb1bf73bafacd0a24b074d0d99ab46
schadof <lars.ragutt@gmx.ch>
2017-01-27 13:35:41 +0100

    Filehandler

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Player player = new Player();
         Menu menu = new Menu();
+        RAWHandler RandW = new RAWHandler("player.txt");
         System.out.println("Welcome to Trails of Agony");
         Scanner in = new Scanner(System.in);
         System.out.println("Name of your adventurer");
         player.name = in.nextLine();
+        RandW.add(player.name, "100");
         System.out.println("Hello brave " + player.name);
         menu.menu(player);
     }
--- src/RAWHandler.java
+++ src/RAWHandler.java
@@ -1,3 +1,79 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class is the heart of the filemanagement system, it reads the Textfiles line by line and splits them into
+ * a String array by semicolons, which means a string of 31380293;ufhuwfj gets split into 31380293 and ufhuwfj
+ * It also writes the data to the textfiles(.txt), and adds new data to the file.
+ */
 public class RAWHandler {
 
+    String file;
+
+    public RAWHandler(String fileName) {
+        file = fileName;
+    }
+
+    //one line at a time
+    String line = null;
+
+    //hashmap
+    Map<String, String> hm = new HashMap();
+
+    public Map<String, String> read() {
+        try {
+            //initialize reader standard encoding
+            FileReader fileReader = new FileReader(file);
+
+            //Wrap FileReader in BufferedReader
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(";");
+                hm.put(parts[0], parts[1]);
+            }
+
+            //Closes BufferedReader
+            bufferedReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("File could not be found");
+            System.out.println("Trying to create file");
+            write();
+        } catch (IOException ex) {
+            System.out.println("Unable to read file");
+        }
+        return hm;
+    }
+
+    //To add a new User
+    public void add(String var1, String var2) {
+        //just in case the file was changed in the meantime
+        read();
+        hm.put(var1, var2);
+        //System.out.println(hm);
+        write();
+    }
+
+    private void write() {
+        try {
+            // Assume default encoding.
+            FileWriter fileWriter = new FileWriter(file);
+
+            // wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            for (Map.Entry<String, String> entry : hm.entrySet()) {
+                bufferedWriter.write(entry.getKey());
+                bufferedWriter.write(";");
+                bufferedWriter.write(entry.getValue());
+                bufferedWriter.newLine();
+            }
+
+            // closes file
+            bufferedWriter.close();
+        } catch (IOException ex) {
+            System.out.println("Error writing to file");
+        }
+    }
 }
